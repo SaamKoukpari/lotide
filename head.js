e573cf64196cd975798832fbd5ff296ee68586a7
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-13 19:16:05 -0700

    added Head

--- head.js
+++ head.js
@@ -4,3 +4,11 @@ const assertEqual = function(actual, expected) {
 
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 1));
+
+const head = function(array) {
+  return array[0]
+
+}
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
