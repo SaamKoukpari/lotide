c995a6c3bb89819a7faebd5e21d1f9afc1079cfe
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-13 18:36:05 -0700

    added assertEqual Function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected ? ("✅✅✅Assertion Passed: " + actual + "===" + expected) : ("🛑🛑🛑Assertion Failed: " + actual + "!==" + expected));
+};
+
+
+
+
+ 
+//   if (actual === expected) {
+//     console.log("✅✅✅Assertion Passed: " + actual + "===" + expected);
+    
+//   } else {
+//     console.log("🛑🛑🛑Assertion Failed: " + actual + "!==" + expected);
+//   }
+// };
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
