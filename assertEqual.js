7b92ef6335f9529168d9dd918eb78dddfe080f8c
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-20 12:25:15 -0700

    refactored code to simplify testing

--- assertEqual.js
+++ assertEqual.js
@@ -2,5 +2,4 @@ const assertEqual = function(actual, expected) {
   return (actual === expected ? (`✅✅✅Assertion Passed: ${actual} === ${expected} `) : (`🛑🛑🛑Assertion Failed: ${actual} !== ${expected} `));
 };
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,9 @@
-const assertEqual = function(actual, expected) {
-  return (actual === expected ? (`✅✅✅Assertion Passed: ${actual} === ${expected} `) : (`🛑🛑🛑Assertion Failed: ${actual} !== ${expected} `));
-};
+const assertEqual = require('./assertEqual');
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
+assertEqual();
 
 const head = function(array) {
   return array[0]
-
 }
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head;
