53fd3ffe126e01a88b70aefc21e7c31bb5a27645
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-20 13:55:38 -0700

    refactored middle and added middleTest

--- middle.js
+++ middle.js
@@ -1,24 +1,6 @@
-const eqArrays = function(array1, array2) {
+const eqArrays = require('./eqArrays');
 
-  if (array1.length !== array2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)){
-    console.log(`✅✅✅Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${array1} !== ${array2}`);
-  }
-}
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   for (var i = 0; i < array.length; i++){
@@ -43,9 +25,4 @@ const middle = function(array) {
   }
 };
 
-console.log(middle([1])) //=> []
-console.log(middle([1, 2])) //=> []
-console.log(middle([1, 2, 3])) //=> [2]
-console.log(middle([1, 2, 3, 4, 5])) //=> [3]
-console.log(middle([1, 2, 3, 4])) //=> [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) //=> [3, 4]
+module.exports = middle;
